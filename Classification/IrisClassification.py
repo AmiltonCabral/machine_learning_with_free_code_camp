95f919eb6f6e8d9f24100f7301f7ae6cbdc55fda
Amilton Cristian <88437789+AmiltonCristian@users.noreply.github.com>
2022-03-20 11:05:49 -0300

    added comments

--- Classification/IrisClassification.py
+++ Classification/IrisClassification.py
@@ -4,17 +4,22 @@ from IPython.display import clear_output
 
 '''
 - We will predict the specie of Iris with sepal and petal measure.
-- The model use DNNClassification with 2 hidden layers and 30, 10
-    nodes respectively.
+- The model use DNNClassification with 2 hidden layers,
+    30, 10 nodes respectively.
 - There are 3 possible species.
 - You can see the accuracy in the last row output.
 '''
 
+
 def data(CSV_PATH):
+    '''
+    Treating our dataframe
+    '''
     header = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
     species = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
     df = pd.read_csv(CSV_PATH, names=header)
 
+    # replacing species names (string) into (integer)
     for i, specie in enumerate(species):
         df['species'] = df['species'].replace(specie, i)
     
@@ -48,11 +53,16 @@ def mk_feature_columns(NUMERIC_COLUMNS):
 def main():
     CSV_PATH = 'https://query.data.world/s/hefzuesvkxqj3gibsxke4nahywcs22'
     NUMERIC_COLUMNS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+        # the train and eval dataframe
     df_train, y_train, df_eval, y_eval = data(CSV_PATH)
     feature_columns = mk_feature_columns(NUMERIC_COLUMNS)
+        # creating the model with 2 hidden layes, 30 and 10 nodes respectively.
     model = tf.estimator.DNNClassifier(feature_columns=feature_columns, hidden_units=[30, 10], n_classes=3)
+        # training the model with 5000 steps
     model.train(input_fn=lambda: input_fn(df_train, y_train), steps=5000)
+        # testing the model
     result = model.evaluate(lambda: input_fn(df_eval, y_eval, False))
+        # printing the accuracy
     print('ACCURACY:', result['accuracy'])
 
 
